8f3c8d5788b3703f5bde31d8b22f5779bd18af4d
IT_6 <baranovmatvey2281@gmail.com>
2025-01-27 15:00:29 +0200

    alalalalllalalalala

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,21 @@
-print('Eney Chorni')
+    def check_password(password):
+    correct_password = 'BogdanLovePolina'
+    if password == correct_password:
+        return True
+    return False
+
+def brute_force(password_list):
+    for password in password_list:
+        print('Checking password: {password}')
+   if check_password(password):
+    print('ACCESS GRANTED ! Password is {password}')
+    return password
+    print('ACCED DENIED! Password is not found!')
+    return None
+
+with open('passwords.txt', 'r') as file:
+    password = file.readlines()
+
+password = [password.strip()for password in password]
+
+brute_force(password)
